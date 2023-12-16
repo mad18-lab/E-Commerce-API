56f2c6048c71466b501a0d624c79ffa0adb90ab6
Madhav <guptamadhav69@gmail.com>
2023-12-16 23:54:16 +0530

    commit changes

--- models/users.js
+++ models/users.js
@@ -42,5 +42,33 @@ const userSchema = new mongoose.Schema({
         timestamps: true
 })
 
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+     const token = jwt.sign({ _id: user._id.toString()},      process.env.JWT_SECRET)
+ user.tokens = user.tokens.concat({token})
+    await user.save()
+    return token
+}
+
+userSchema.pre('save', async function(next) {
+    const user = this
+       if (user.isModified('password')) {
+       user.password = await bcrypt.hash(user.password, 11)
+    }
+      next()
+})
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await Users.findOne({ email })
+    if (!user) {
+      throw new Error('Unable to log in')
+    }
+     const isMatch = await bcrypt.compare(password, user.password)
+    if(!isMatch) {
+       throw new Error('Unable to login')
+    }
+       return user
+    }
+
 const Users = mongoose.model('Users', userSchema)
 module.exports(Users)
---
+++ routes/users.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const users = require('../models/users');
+
+const route = new express.Router()
+
+route.post('/users', async (req, res) => {
+    const users = new Users(req.body)
+    try {
+        await user.save()
+        const token = await users.generateAuthToken()
+        res.status(201).send({users, token})
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+route.post('/users/login', async (req, res) => {
+    try {
+      const user = await Users.findByCredentials(req.body.email, req.body.password)
+      const token = await user.generateAuthToken()
+      res.send({ user, token})
+    } catch (error) {
+      res.status(400).send(error)
+     }
+})
+
+route.post('/users/logout', Auth, async (req, res) => {
+    try {
+       req.user.tokens =  req.user.tokens.filter((token) => {
+            return token.token !== req.token 
+        })
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+route.post('/users/logoutAll', Auth, async(req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send()
+    } catch (error) {
+        res.status(500).send()        
+    }
+})
+
+module.exports = route
